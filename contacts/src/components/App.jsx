11a9ce90d66f1020fb63978c8fc437fd99c90c6b
namya5 <77522983+namya5@users.noreply.github.com>
2023-07-07 19:41:44 +0530

    Add files via upload

--- contacts/src/components/App.jsx
+++ contacts/src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Card from "./card"
 import contacts from "../contacts"
 
+function createCard(contact)
+{
+    return (
+        <Card key = {contact.id} name = {contact.name} img = {contact.imgURL} tell = {contact.tell} email = {contact.email}/>
+    );
+}
 
 function App() {
     return (
       <div>
         <h1 className="heading">My Contacts</h1>
-        <Card name={contacts[0].name} img={contacts[0].imgURL} tell={contacts[0].tell} email={contacts[0].email}/>
-        <Card name={contacts[1].name} img={contacts[1].imgURL} tell={contacts[1].tell} email={contacts[1].email}/>
-        <Card name={contacts[2].name} img={contacts[2].imgURL} tell={contacts[2].tell} email={contacts[2].email}/>
+        {contacts.map(createCard)}
       </div>
     );
   }
